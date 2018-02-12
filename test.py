9296ae3f0ca0552cfa728bfbdc56655e86a543dc
beninato8 <beninato8@gmail.com>
2018-02-12 09:28:40 -0800

    testing for substrings

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+"""
 compare = ['purple', 'yellow', 'blue green',
            'orange', 'yellow-green']
 
@@ -13,4 +14,84 @@ print(c)
 
 for i in range(10):
     print(i, i % 3)
+"""
 
+fruits = ['Apple', 
+'Apricot', 
+'Avocado', 
+'Banana', 
+'Berries', 
+'Breadfruit', 
+'Carob', 
+'Cherry', 
+'Citron', 
+'Coconut', 
+'Date', 
+'Dragon Fruit', 
+'Durian', 
+'Fig', 
+'Ginger', 
+'Grapes', 
+'Currant', 
+'Raisin', 
+'Grapefruit', 
+'Guava', 
+'Jackfruit', 
+'Jujube', 
+'Kiwifruit', 
+'Kumquat', 
+'Lemon', 
+'Lime', 
+'Longan', 
+'Loquat', 
+'Lucuma', 
+'Lychee', 
+'Mamey Sapote', 
+'Mango', 
+'Mangosteen', 
+'Melons', 
+'Nance', 
+'Nectarine', 
+'Noni', 
+'Oranges', 
+'Blood Orange', 
+'Mandarin', 
+'Navel', 
+'Seville', 
+'Valencia', 
+'Papaya', 
+'Passion Fruit', 
+'Peach', 
+'Pear', 
+'Asian Pear', 
+'Persimmon', 
+'Pineapple', 
+'Plantain', 
+'Plum', 
+'Damson', 
+'Prunes', 
+'Pomegranate', 
+'Pomelo', 
+'Prickly Pear', 
+'Quince', 
+'Rambutan', 
+'Rhubarb', 
+'Starfruit', 
+'Tamarillo', 
+'Tamarind', 
+'Tangerine', 
+'Tangelo', 
+'Tomato']
+
+res = []
+search = 'x,y,z'
+for x in fruits:
+    for y in search:
+        if y.lower() in x.lower() and x not in res:
+            res.append(x)
+
+print(res)
+
+print(list(x for x in fruits for y in search.split(',') if y.lower() in x.lower()))
+
+print("hello".split(','))
