3efa01cd463ad1c014576bc353229605b00ea4f7
beninato8 <beninato8@gmail.com>
2018-02-12 13:02:46 -0800

    updating script

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo hello
+#bash <(curl -s https://raw.githubusercontent.com/beninato8/phoenix/master/install.sh)
+echo "Installing Phoenix Ship Searcher"
+mkdir -p ~/Github/
+cd ~/Github/
+git clone https://github.com/beninato8/phoenix.git
+echo "Installation Done!"
